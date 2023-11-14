47879a22c88797bed195ba853eb70f3ee3c2447f
nichuanfang <f18326186224@gmail.com>
2023-11-14 22:50:30 +0800

    ✨ feat: 更新

--- tgbot_handle.py
+++ tgbot_handle.py
@@ -1,7 +1,12 @@
 # 处理tgbot的请求
 import sys
 
-# 主题数据
-topic_data = sys.argv[1]
+# payload_type
+payload_type = sys.argv[1]
+# tgbot_payload
+tgbot_payload = sys.argv[2]
 
-print('hello world')
+if payload_type == 'update_topic_repo':
+    print('update_topic_repo已成功触发')
+else:
+    print(payload_type)
