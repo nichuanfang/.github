f758ab4079c7fa1d9db4a93c46aa432a1b9542bd
nichuanfang <f18326186224@gmail.com>
2023-11-14 23:11:17 +0800

    ✨ feat: 更新

--- tgbot_handle.py
+++ tgbot_handle.py
@@ -1,10 +1,15 @@
 # 处理tgbot的请求
 import sys
 import json
+import base64
 
 # topic_payload
 topic_payload = sys.argv[1]
 
-# payload = json.loads(topic_payload)
+if topic_payload:
+    topic_payload = base64.b64decode(topic_payload).decode('utf-8')
+    payload = json.loads(topic_payload)
+else:
+    raise Exception('topic_payload is empty')
 
-print(topic_payload)
+print(payload['topic'])
